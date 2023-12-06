a0ede7dca2982c8e082a586b418d3551463749cf
dmdu1220 <dmitrii.dudukin@netcracker.com>
2023-12-06 11:46:34 +0600

    Init commit

---
+++ src/main/java/com/dud/and/kra/astrology/AstrologyApplication.java
@@ -0,0 +1,13 @@
+package com.dud.and.kra.astrology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrologyApplication.class, args);
+	}
+
+}
